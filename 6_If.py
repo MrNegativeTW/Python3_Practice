3437825e5fb73ca92788c63a011c60bf45ccdc4f
MrNegativeTW <wowpapa3232@gmail.com>
2018-07-17 21:55:00 +0800

    67

--- 6_If.py
+++ 6_If.py
@@ -15,22 +15,39 @@ else:
 print('-----Logical Operators-----')
 
 user = 'root'
-login_stats = True
+login_stats = True  # Change True or False
 
-if user == 'root' and login_stats == True:
+if user == 'root' and login_stats:
 	print('Welcome! Root User')
 else:
 	print('Fuck Off, you\'re not root user')
 
 # False Values
 print('-----False Values-----')
+if not login_stats:
+	print('Fuck off')
+else:
+	print('Welcome')
 
+# Identity Operators
+print('-----Identity Operators-----')
+a = ['a', 'b', 'c']
+b = ['a', 'b', 'c']
 
+print(id(a))
+print(id(b))
+print(a == b)
+print(a is b) # Result = False, use b = a to reverse result
 
+# False Values
+print('-----False Values-----')
+# All of these False / None / 0 / '' / () / [] / {}, equals to False
+condition = 'ChangeMe'
 
-
-
-
+if condition:
+	print('Equal to True')
+else:
+	print('Equal ot False')
 
 
 
