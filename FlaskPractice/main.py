4be0a5dbd0433c307a2da447ecd609e072000d24
MrNegativeTW <wowpapa3232@gmail.com>
2018-09-10 23:07:45 +0800

    超難的Flask

--- FlaskPractice/main.py
+++ FlaskPractice/main.py
@@ -3,13 +3,6 @@ from flask import Flask, render_template, url_for, flash, request
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, SubmitField
 
-class TestForm(FlaskForm):
-	number = SelectField('有幾項呢', choices=[('1','1 項'), ('2','2 項')])
-	item = StringField('Name')
-	add = SubmitField('加入')
-	submit = SubmitField('幫我選一個吧')
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a83c51c8b7fc804eb395d7c1d753fa28'
 
@@ -18,16 +11,29 @@ app.config['SECRET_KEY'] = 'a83c51c8b7fc804eb395d7c1d753fa28'
 def hello():
 	return render_template('index.html')
 
+class TestForm(FlaskForm):
+	number = SelectField('有幾項呢', choices=[('1','1 項'), ('2','2 項')])
+	item = StringField('Name')
+	add = SubmitField(label='加入')
+	submit = SubmitField('幫我選一個吧')
+
 # 神選之物
 @app.route('/DiffToChoice', methods=['GET', 'POST'])
 def DiffToChoice():
 	form = TestForm()
+
 	qu=[]
 	itemss = form.item.data
-	if request.method == 'POST':
-		qu.append(itemss)
 
-	return render_template('DiffToChoice.html', form=form)
+	if request.method == 'POST':
+		if request.form['submit'] == 'Do':
+			qu.append(itemss)
+		elif request.form['submit'] == 'NotDo':
+			return render_template('ToGoogle.html', form=form)
+		else:
+			pass
+	elif request.method == 'GET':
+		return render_template('DiffToChoice.html', form=form)
 
 
 
