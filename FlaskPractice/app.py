43fd2d7fd5701a0f2b8f9504f30b286eebd828ab
MrNegativeTW <wowpapa3232@gmail.com>
2018-09-14 23:05:30 +0800

    Change Validate Way
    from request.method to validate_on_submit

--- FlaskPractice/app.py
+++ FlaskPractice/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, flash, request
-from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField
+
+# Custom WTFroms
+from forms import DiffToChoiceForm, GasPricePerKmForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a83c51c8b7fc804eb395d7c1d753fa28'
@@ -11,12 +12,6 @@ def hello():
 	return render_template('index.html')
 
 # 神選之物
-class DiffToChoiceForm(FlaskForm):
-	number = SelectField('有幾個東西讓你選擇障礙呢：', choices=[('2','2 項'), ('3','3 項')])
-	itemOne = StringField('第一項')
-	itemTwo = StringField('第二項')
-	# add = SubmitField(label='加入')
-	submit = SubmitField('神啊 幫我選一個')
 @app.route('/DiffToChoice', methods=['GET', 'POST'])
 def DiffToChoice():
 	import random
@@ -33,35 +28,39 @@ def DiffToChoice():
 		qu.append(itemssOne)
 		qu.append(itemssTwo)
 		ans = random.choice(qu)
-		return render_template('DiffToChoice.html', form=form, ans=ans)
+		return render_template('DiffToChoice.html',title='油耗計算', form=form, ans=ans)
 
 # ToGoogle
 @app.route('/ToGoogle')
 def ToGoogle():
 	import webbrowser
 	webbrowser.open('https://google.com')
-	return render_template('ToGoogle.html')
+	return render_template('ToGoogle.html', title='ToGoogle')
 
 # 油耗計算 / GasPricePerKm
-class GasPricePerKmForm(FlaskForm):
-	mileage = StringField('Trip 里程數')
-	oil = StringField('加了多少公升 ?')
-	submit = SubmitField('幫我算算')
 @app.route('/GasPricePerKm', methods=['GET', 'POST'])
 def GasPricePerKm():
 	form = GasPricePerKmForm()
 
-	x = form.mileage.data
-	y = form.oil.data
-
-	if request.method == 'GET':
-		return render_template('GasPricePerKm.html', form=form)
-	elif request.method == 'POST':
-		xx = float(x)
-		yy = float(y)
-		result = xx / yy
+	if form.validate_on_submit():
+		x = float(form.mileage.data)
+		y = float(form.oil.data)
+		result = x / y
 		results = int(result)
-		return render_template('GasPricePerKm.html', form=form, result=results)
+		return render_template('GasPricePerKm.html',title='油耗計算', form=form, result=results)
+
+	return render_template('GasPricePerKm.html',title='油耗計算', form=form)
+
+	# if request.method == 'GET':
+	# 	return render_template('GasPricePerKm.html',title='油耗計算', form=form)
+	# elif request.method == 'POST':
+	# 	x = form.mileage.data
+	# 	y = form.oil.data
+	# 	xx = float(x)
+	# 	yy = float(y)
+	# 	result = xx / yy
+	# 	results = int(result)
+	# 	return render_template('GasPricePerKm.html',title='油耗計算', form=form, result=results)
 
 # 漂亮的考試時間 / BeautifulExam
 @app.route('/BeautifulExam', methods=['GET', 'POST'])
---
+++ FlaskPractice/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, StringField, SubmitField, IntegerField, FloatField, DecimalField
+from wtforms.validators import DataRequired, InputRequired, Length, NumberRange, Regexp
+
+# 神選之物
+class DiffToChoiceForm(FlaskForm):
+	number = SelectField(
+		'選擇障礙數量：（開發中）', 
+		choices=[('2','2 項'), ('3','3 項')
+		])
+	itemOne = StringField('第一項')
+	itemTwo = StringField('第二項')
+	# add = SubmitField(label='加入')
+	submit = SubmitField('神啊 幫我選一個')
+
+# 油耗計算 / GasPricePerKm
+class GasPricePerKmForm(FlaskForm):
+	mileage = StringField(
+		'Trip 里程數',
+		validators=[
+			DataRequired(),
+			Regexp(regex='\d+',message='數字啦幹')
+		])
+	oil = StringField(
+		'加了多少公升 ?',
+		validators=[
+			DataRequired(),
+			Regexp(regex='\d+',message='數字啦幹')
+		])
+	submit = SubmitField('幫我算算')
