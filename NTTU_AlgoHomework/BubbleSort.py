130aba7795919db76861f36a2bd46a8fef3a6878
MrNegativeTW <wowpapa3232@gmail.com>
2018-09-20 22:51:59 +0800

    BubbleSort
    Bubble Sort Done
    完成氣泡排序，雖然不是我的作業

--- NTTU_AlgoHomework/BubbleSort.py
+++ NTTU_AlgoHomework/BubbleSort.py
@@ -13,30 +13,25 @@ else:
 print('輸入測試數量：', end='')
 number = int(input())
 
-# Empty List
+# Empty List / Number of items / swap times
 arr = []
 count = 1
+times = 0
 
+# User Enter
 while count <= number:
 	print('請輸入第',count,'項：', end='')
 	arr.append(int(input()))
 	count += 1
 
-# Main Program Start Here
-def bubbleSort(arr):
-    # Length of List, how many times to loop
-    for i in range(len(arr)):
-        # echo every elements in list
-        for j in range(0, len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+# Length of List, how many times to loop
+for i in range(0, len(arr)-1):
+	# echo every elements in list
+	for j in range(0, len(arr)-i-1):
+        # Change Order
+		if arr[j] > arr[j+1]:
+			arr[j], arr[j+1] = arr[j+1], arr[j]
+			times += 1
 
-bubbleSort(arr)
-print('-------------------')
-print ('重新排序後為：', end=' ')
-# Print prettyify numbers
-# for i in range(len(arr)):
-# 	print (arr[i], end=' ')
-
-# Print List Direct
-# print(arr)
+print('---------- Results ----------')
+print ('Optimal train swapping takes', times, 'swaps')
