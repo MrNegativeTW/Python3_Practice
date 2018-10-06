a20607fb75d8af173eda0e0d4446f7f8fd039f63
TrevorWu <wowpapa3232@gmail.com>
2018-10-06 13:20:42 +0800

    1-2
    不知道哪來的檔案

---
+++ NTTU_AlgoHomework/HW1-2.py
@@ -0,0 +1,12 @@
+﻿class Solution(object):
+    def countPrimes(self, n):
+        if n < 3:
+            return 0
+        primes = True * n
+        primes[0] = primes[1] = False
+        """循环的范围进行了缩小"""
+        for i in range(2, int(n ** 0.5) + 1):
+            """先判断是否已经变为false，同时从i*i开始进行遍历"""
+            if primes[i]:
+                primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
+        return sum(primes)
