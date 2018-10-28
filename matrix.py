1c9198474234f944e627b86c43bd6f99002e8521
TrevorWu <wowpapa3232@gmail.com>
2018-10-28 16:53:37 +0800

    matrix.py

---
+++ matrix.py
@@ -0,0 +1,34 @@
+def magicSquare(n):
+	square = []
+	for i in range(n + 1):
+		square.append([0] * (n + 1))
+
+	i = 0
+	j = (n + 1) // 2
+
+	for key in range(1, n ** 2 + 1):
+		if key % n == 1:
+			i += 1
+		else:
+			i -= 1
+			j += 1
+		if i == 0:
+			i = n
+		if j > n:
+			j = 1
+		square[i][j] = key
+
+	matrix = []
+
+	for i in range(n):
+		matrix.append([0] * n)
+
+	for k in range(len(matrix)):
+		for l in range(len(matrix[0])):
+			matrix[k][l] = square[k + 1][l + 1]
+
+	return matrix
+
+matrix = magicSquare(int(input()))
+for result in matrix:
+	print(result)
