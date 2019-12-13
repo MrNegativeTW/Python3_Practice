e06ecf2a2585d0c38a0defacc7337acb7c3f98f1
TrevorWu <wowpapa3232@gmail.com>
2019-12-13 20:55:55 +0800

    更新 ChiSquareDistribution.py
    - 格式化，加入更多空格。
    - 移除版本需求。
    - 加入作業標題。

--- SomeShitsHere/ChiSquareDistribution.py
+++ SomeShitsHere/ChiSquareDistribution.py
@@ -1,30 +1,29 @@
+# 統計學作業 2019/12/09
+# ------------------------------
 import numpy as numpy
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-# numpy == 1.17.4
-# scipy == 1.3.3
-# matplotlib = 3.1.2
 
 # Normal Distribution
 norm = stats.norm(0, 1)
 
 # Degree of freedom: 4
 # Number of samples: 100,000
-x1 = norm.rvs(size=100000)**2
-x2 = norm.rvs(size=100000)**2
-x3 = norm.rvs(size=100000)**2
-x4 = norm.rvs(size=100000)**2
+x1 = norm.rvs(size = 100000)**2
+x2 = norm.rvs(size = 100000)**2
+x3 = norm.rvs(size = 100000)**2
+x4 = norm.rvs(size = 100000)**2
 
 # Add each of them
 y = x1 + x2 + x3 + x4
 
 
 # Plot the histogram
-plt.hist(y, 30, normed=True, color='c')
+plt.hist(y, 30, normed = True, color = 'c')
 
 # Plot the curve
 x = numpy.arange(0, 30, .05)
-plt.plot(x, stats.chi2.pdf(x, df=4), color='r', lw=2)
+plt.plot(x, stats.chi2.pdf(x, df = 4), color = 'r', lw = 2)
 
 
 # Fire it up
